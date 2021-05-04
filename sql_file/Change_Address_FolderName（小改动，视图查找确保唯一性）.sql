3cf40828da018c2f3fab150276701f0e6eb26d60
DyllanElliia <53618778+DyllanElliia@users.noreply.github.com>
2021-05-04 16:01:39 +0800

    Update Change_Address_FolderName（小改动，视图查找确保唯一性）.sql

--- sql_file/Change_Address_FolderName（小改动，视图查找确保唯一性）.sql
+++ sql_file/Change_Address_FolderName（小改动，视图查找确保唯一性）.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE FUNCTION Change_Address_FolderName( pictureAddress_ varchar,
+CREATE OR REPLACE FUNCTION Change_Address_FolderName
+( pictureAddress_ varchar,
 													userName_ varchar,
 													 newPictureAddress_ varchar,
 													new_folder_name_ varchar)
@@ -8,36 +9,46 @@ declare
     Pictureid_ integer;
     userid_ integer;
 	newFolderID integer;
-	
 BEGIN
-		
-	select user_id into userid_ from user_ where user_name=userName_; 
-	
-	select picture_id into Pictureid_ from all_show where user_id=userid_ and picture_address=pictureAddress_;
-	
-	select folder_id into newFolderID from all_show 
-	where folder_name=new_folder_name_ and user_id=userid_ ;
-	
-	
-	
-	if newFolderID is NULL
-	then 
-		insert into folder_ values (default,new_folder_name_,userid_);
-	end if;
-	
-	select folder_id into newFolderID from folder_ 
-	where folder_name=new_folder_name_ and user_id=userid_;
-	
-	if newFolderID IS NULL OR userid_ IS NULL OR Pictureid_ is NULL
-	 
+    select user_id
+    into userid_
+    from user_
+    where user_name=userName_;
+
+    select picture_id
+    into Pictureid_
+    from all_show
+    where user_id=userid_ and picture_address=pictureAddress_;
+
+    select folder_id
+    into newFolderID
+    from all_show
+    where folder_name=new_folder_name_ and user_id=userid_
+    ;
+
+    if newFolderID is NULL
+	then
+    insert into folder_
+    values
+        (default, new_folder_name_, userid_);
+end
+if;
+
+	select folder_id
+into newFolderID
+from folder_
+where folder_name=new_folder_name_ and user_id=userid_;
+
+if newFolderID IS NULL OR userid_ IS NULL OR Pictureid_ is NULL
     THEN
-    return FALSE;
-	END IF;
-	
+return FALSE;
+END
+IF;
+
 	UPDATE picture_ SET picture_address = newPictureAddress_, folder_id = newFolderID
         WHERE picture_id=Pictureid_;
-		
-    return TRUE;
+
+return TRUE;
 END
 $$language plpgsql;
 	
